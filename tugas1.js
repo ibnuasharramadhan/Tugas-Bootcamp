ab90c31e4ca26606d60ab5763e56c23aae71a251
Ibnu Ashar <ibnuashar9@gmail.com>
2019-07-14 14:43:32 +0700

    Tugas pertama

---
+++ tugas1.js
@@ -0,0 +1,23 @@
+//tipe data boolean
+var sudahTugas = true;
+var belumTugas = false;
+
+console.log(sudahTugas);//true
+console.log(belumTugas);
+
+//tipe data array
+var laptop = ["Asus","Lenovo","Acer","Macbook","Zyrex"];
+
+console.log(laptop);
+console.log(laptop[3]);
+console.log("Data Terakhir: " + laptop[laptop.length -1]);
+console.log("Total: " + laptop.length);
+
+//tipe object
+var motor = {
+ merk : "Honda"
+ nama : "Supra"
+ nopol : "B 4433 KJO"
+ warna : [" Silver, Hitam, Merah"]
+}; 
+console.log(motor);
