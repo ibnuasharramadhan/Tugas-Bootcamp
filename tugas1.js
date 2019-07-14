c649426782f710c0094cd26321f42148a4e386bd
Ibnu Ashar <ibnuashar9@gmail.com>
2019-07-14 14:56:45 +0700

    Tugas Selesai

--- tugas1.js
+++ tugas1.js
@@ -20,4 +20,47 @@ var motor = {
  nopol : "B 4433 KJO"
  warna : [" Silver, Hitam, Merah"]
 }; 
-console.log(motor);
+console.log("nama pelanggan :", motor.merk);
+console.log("nama motor :", motor.nama);
+console.log("nomor polisi :", motor.nopol);
+console.log("warna motor :", motor.[2]);
+
+//array of object
+var dosen = [
+    {
+        name: "Dedi",
+        age: 38,
+        matkul: "Pemograman"
+    },
+    {
+        name: "Hedy",
+        age: 39,
+        address: "Jogja"
+    }
+];
+
+console.log(dosen);
+
+
+//No.2
+var kartuKeluarga = [
+    {
+        name: "Ibnu AShar",
+        address: "Bekasi Timur",
+        gender: "Male"
+    },
+    {
+        name: "Yerri Dwiandra",
+        address: "Bekasi Barat",
+        gender: "Female"
+    }
+];
+
+//console.log([kartuKeluarga[0].name + " - " + kartuKeluarga[0].address + " - " + kartuKeluarga[0].gender]);
+
+var i = 0;
+while(i < kartuKeluarga.length) {
+    var no = i+1;
+    console.log(no++ + " " + kartuKeluarga[i].name + " - " + kartuKeluarga[i].address + " - " + kartuKeluarga[i].gender);
+    i++;
+};
